286a679408ed0d969feaae47bf06982376dcc871
ZeiZel <valera2003lvov@yandex.ru>
2023-05-26 07:17:59 +0300

    added filters

--- client-side/page-components/ProductsList/ProductsList.tsx
+++ client-side/page-components/ProductsList/ProductsList.tsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useGetProductsQuery } from '../../store/product/product.api';
 import { catalogPageData } from '../../helpers';
-import { Card, HTag, Spinner } from '../../components';
+import { Button, Card, HTag, Spinner } from '../../components';
 import { ErrorPage } from '../ErrorPage/ErrorPage';
 import Head from 'next/head';
 import { IProduct } from '../../store/product/product.interface';
-import { ProductCard } from '../../components/ProductCard/ProductCard';
-import Error500 from '../../pages/500';
+import { ProductCard } from '../../components';
 import styles from './ProductsList.module.scss';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
+import { notify } from '../../helpers/tostify';
 
 export const ProductsList = () => {
 	const [page, setPage] = useState<number>(1);
@@ -27,6 +29,68 @@ export const ProductsList = () => {
 		page,
 	});
 
+	const [minPrice, setMinPrice] = useState<number>(0);
+	const [maxPrice, setMaxPrice] = useState(10000);
+	const [inStock, setInStock] = useState<boolean>(false);
+	const [filteredProducts, setFilteredProducts] = useState(products);
+
+	const setMaxPriceValue = () => {
+		if (products) {
+			let max = 0;
+			for (let i = 0; i < products.length; i++) {
+				if (products[i].price > max) {
+					max = products[i].price;
+				}
+			}
+			return max;
+		}
+		return 0;
+	};
+
+	const setMinPriceValue = () => {
+		if (products) {
+			let min = Infinity;
+			for (let i = 0; i < products.length; i++) {
+				if (products[i].price < min) {
+					min = products[i].price;
+				}
+			}
+			return min;
+		}
+		return 0;
+	};
+
+	const generateProducts = () =>
+		category && filteredProducts && filteredProducts.length > 0 ? (
+			filteredProducts.map((product: IProduct) => (
+				<ProductCard key={product._id} product={product} useModal={true} />
+			))
+		) : (
+			<HTag tag={'h1'}>Товары не найдены</HTag>
+		);
+
+	const handleFilter = () => {
+		const filteredProducts =
+			category &&
+			products &&
+			products.filter((product) => {
+				return (
+					!(product.price < minPrice || product.price > maxPrice) &&
+					(inStock ? product.count > 0 : true)
+				);
+			});
+		setFilteredProducts(filteredProducts);
+	};
+
+	useEffect(() => {
+		setMaxPrice(setMaxPriceValue());
+		setMinPrice(setMinPriceValue());
+	}, [products]);
+
+	useEffect(() => {
+		handleFilter();
+	}, [minPrice, maxPrice, inStock]);
+
 	// произвести сравнение
 	const category = catalogPageData[Number(alias)];
 
@@ -38,38 +102,48 @@ export const ProductsList = () => {
 		return <ErrorPage type={'404'} />;
 	}
 
-	if (category && products) {
-		return (
-			<div className={styles['products-list']}>
-				<Head>
-					<title>{category.title}</title>
-				</Head>
-				<div className={styles.container}>
-					<div className={styles['products-list__wrapper']}>
-						<Card className={styles['products-list__filters']}>
-							<HTag tag={'h2'}>Фильтры</HTag>
-						</Card>
-						<Card color={'black'} className={styles['products-list__cards']}>
-							<HTag tag={'h1'}>{category.title}</HTag>
-							<div className={styles['products-list__items']}>
-								{products.length > 0 ? (
-									products.map((product: IProduct) => (
-										<ProductCard
-											key={product._id}
-											product={product}
-											useModal={true}
-										/>
-									))
-								) : (
-									<HTag tag={'h1'}>Товаров данной категории нет</HTag>
-								)}
-							</div>
-						</Card>
-					</div>
+	return (
+		<div className={styles['products-list']}>
+			<Head>
+				<title>{category.title}</title>
+			</Head>
+			<div className={styles.container}>
+				<div className={styles['products-list__wrapper']}>
+					<Card className={styles['products-list__filters']}>
+						<HTag tag={'h2'}>Фильтры</HTag>
+						<InputRange
+							minValue={0}
+							maxValue={5000}
+							value={{ min: minPrice, max: maxPrice }}
+							onChange={(value) => {
+								setMinPrice(value.min);
+								setMaxPrice(value.max);
+							}}
+							onChangeComplete={handleFilter}
+						/>
+						<label className={styles.label} htmlFor={'inStock'}>
+							<input
+								id={'inStock'}
+								type='checkbox'
+								checked={inStock}
+								onChange={(e) => {
+									setInStock(!inStock);
+									handleFilter();
+								}}
+							/>
+							{'  '}
+							Только в наличии
+						</label>
+						<Button arrow={'none'} appearance={'primary'} onClick={handleFilter}>
+							Применить
+						</Button>
+					</Card>
+					<Card color={'black'} className={styles['products-list__cards']}>
+						<HTag tag={'h1'}>{category.title}</HTag>
+						<div className={styles['products-list__items']}>{generateProducts()}</div>
+					</Card>
 				</div>
 			</div>
-		);
-	}
-
-	return <Error500 />;
+		</div>
+	);
 };
