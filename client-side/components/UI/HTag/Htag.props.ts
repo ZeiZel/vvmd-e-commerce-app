607778947bf6df7ad53f822c236dc76c8339a166
Lvov Valery <69900055+ZeiZel@users.noreply.github.com>
2023-06-27 16:05:33 +0300

    Update Htag.props.ts

--- client-side/components/UI/HTag/Htag.props.ts
+++ client-side/components/UI/HTag/Htag.props.ts
@@ -1,7 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
 export interface ITagProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLElementTagNameMap>, HTMLElementTagNameMap> {
+	extends DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {
 	tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 	children: ReactNode;
 }
